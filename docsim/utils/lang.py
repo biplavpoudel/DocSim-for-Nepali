2fdd525a8ab75180b1b9b4ccc4527579f45e625d
Biplav Poudel <biplavpoudel764@gmail.com>
2024-06-10 15:50:02 +0545

    Modified for Nepali

--- docsim/utils/lang.py
+++ docsim/utils/lang.py
@@ -8,22 +8,33 @@ ISO639_TO_SCRIPT = {
     'ml': 'malayalam',
     'te': 'telugu',
     'kn': 'kannada',
-    'or': 'oriya',
-    'bn': 'bengali',
-    'as': 'bengali',
-    'gu': 'gujarati',
     'hi': 'devanagari',
-    'mr': 'devanagari',
-    'ne': 'devanagari',
-    'pa': 'gurmukhi'
+    'nep': 'devanagari',
 }
 
 MAX_RANGE = 1114112
+MAX_DEVANAGARI =43264
+MIN_DEVANAGARI = 2304
 ALLOWED_CATEGORIES = ['L', 'M', 'N', 'P']
+DISALLOWED_CHARACTERS = ['ऀ', 'ऌ', 'ऍ', 'ऎ','ऄ','ऑ', 'ऒ','ऩ',
+                         'ऱ', 'ळ', 'ऴ', 'ऺ', 'ऻ', '़', 'ऽ',
+                         'ॅ', 'ॆ', 'े', 'ै', 'ॎ', 'ॏ',
+                         '॑', '॒', '॓', '॔', 'ॕ', 'ॖ', 'ॗ',
+                         'क़', 'ख़', 'ग़', 'ज़', 'ड़', 'ढ़', 'फ़',
+                         'य़', 'ॠ', 'ॡ', 'ॢ', 'ॣ','ृ',
+                         'ॱ', 'ॲ', 'ॳ', 'ॴ', 'ॵ', 'ॶ', 'ॷ',
+                         'ॸ', 'ॹ', 'ॺ', 'ॻ', 'ॼ', 'ॽ', 'ॾ', 'ॿ',
+                         'ꣲ', 'ꣳ', 'ꣴ', 'ꣵ','ॉ', 'ॊ',
+                         'ꣶ', 'ꣷ', 'ꣻ', 'ꣽ', 'ꣾ', 'ꣿ']
+# ALLOWED_CATEGORIES = ['L', 'M', 'N', 'P', 'S', 'Z', 'C']
 def get_characters(script_name, only_prefix_match=False, skip_punctuations=False, skip_numbers=False, verbose=True):
     characters = {}
+    unicode_list = []
+    unicode = []
+    counter = 0
+    categories = []
     script_name = script_name.upper()
-    for i in range(MAX_RANGE):
+    for i in range(MIN_DEVANAGARI, MAX_DEVANAGARI):
         try:
             name = ud.name(chr(i))
         except ValueError:
@@ -33,20 +44,36 @@ def get_characters(script_name, only_prefix_match=False, skip_punctuations=False
                 continue
         elif script_name not in name:
             continue
-        
+        if script_name in name:
+            unicode.append(i)
+            unicode_list.append(name)
+            counter += 1
+
         # Got a character of that script
         cat = ud.category(chr(i))
+        categories.append(cat)
         if skip_numbers and cat[0] == 'N':
             continue
         if skip_punctuations and cat[0] == 'P':
             continue
-        
+
         if cat[0] not in ALLOWED_CATEGORIES:
             if verbose:
                 print('SKIPPED:', chr(i), name, cat)
         else:
             characters[chr(i)] = (name, cat)
-        
+    # print("The correct unicodes are: ", unicode_list)
+    # print("The number of characters: ", counter)
+    # print("The range of devanagari unicodes are:", unicode)
+    # print("The categories are:", categories)
+    # print(characters.keys())
+    filtered_dict = filter_characters(characters)
+    return filtered_dict
+
+def filter_characters(characters):
+    keys_to_remove = [key for key in characters if key in DISALLOWED_CHARACTERS]
+    for item in keys_to_remove:
+        del characters[item]
     return characters
 
 def get_vowels(characters):
@@ -71,33 +98,33 @@ class EnglishCharacters:
             self.characters += string.punctuation
         if not skip_numbers:
             self.characters += string.digits
-        
+
         self.vowels = 'AEIOU'
         self.consonants = 'BCDFGHJKLMNPQRSTVWXYZ'
 
 class LanguageCharacters:
-    def __init__(self, lang_code, only_prefix_match=True, skip_punctuations=True, skip_numbers=True, 
+    def __init__(self, lang_code, only_prefix_match=True, skip_punctuations=True, skip_numbers=True,
                  verbose=False):
-        
+
         if lang_code not in ISO639_TO_SCRIPT:
             exit('Lang code %s not found' % lang_code)
-        
+
         self.lang_code = lang_code
         self.script = ISO639_TO_SCRIPT[lang_code]
         self.characters = get_characters(self.script, only_prefix_match, skip_punctuations, skip_numbers, verbose)
         self.vowels = get_vowels(self.characters)
         self.consonants = get_consonants(self.characters)
         self.import_letter_combinations()
-        
+
         if verbose: self.print_details()
-        
+
     def import_letter_combinations(self):
         if self.lang_code == 'ta':
             import tamil
             self.letter_combinations = tamil.utf8.tamil247
-        
+
         return
-    
+
     def print_details(self):
         print('LANGUAGE CODE:', self.lang_code)
         print('SCRIPT NAME:', self.script)
@@ -105,7 +132,7 @@ class LanguageCharacters:
         print('VOWELS:'); print(self.vowels)
         print('CONSONANTS:'); print(self.consonants)
         return
-        
+
 
 if __name__ == '__main__':
-    c = LanguageCharacters('ta', only_prefix_match=True, skip_punctuations=True, skip_numbers=True, verbose=True)
+    c = LanguageCharacters('nep', only_prefix_match=True, skip_punctuations=True, skip_numbers=True, verbose=True)
